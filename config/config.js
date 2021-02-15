2e048861111a859a7fedf68b3aaea46d4ded557e
Karmen Kukk <karmenkukk91@gmail.com>
2021-02-15 20:09:21 +0200

    Move database

--- config/config.js
+++ config/config.js
@@ -16,6 +16,20 @@ module.exports = {
       }
     }
   },
+  "production": {
+    "username": process.env.PG_USERNAME,
+    "password": process.env.PG_PASSWORD,
+    "database": process.env.PG_DATABASE,
+    "host": process.env.PG_HOST,
+    "port": process.env.PG_PORT,
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+      }
+    }
+  },
   "test": {
     "username": process.env.PG_TEST_USERNAME,
     "password": process.env.PG_TEST_PASSWORD,
--- src/db.config.js
+++ src/db.config.js
@@ -13,6 +13,20 @@ const config = {
       idle: 60000
     }
   },
+  "production": {
+    HOST: process.env.PG_HOST,
+    PORT: process.env.PG_PORT,
+    USER: process.env.PG_USERNAME,
+    PASSWORD: process.env.PG_PASSWORD,
+    DB: process.env.PG_DATABASE,
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 60000
+    }
+  },
   "test": {
     HOST: process.env.PG_TEST_HOST,
     PORT: process.env.PG_TEST_PORT,
